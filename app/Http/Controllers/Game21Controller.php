410da07acd524157eb3d4b0c5de935027f3213be
Erik Rundberg <erik@rundberg.ax>
2021-05-25 21:08:11 +0200

    phpstan fixed

--- app/Http/Controllers/Game21Controller.php
+++ app/Http/Controllers/Game21Controller.php
@@ -7,12 +7,6 @@ use Illuminate\Http\Request;
 
 class Game21Controller extends Controller
 {
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Game21  $game21
-     * @return \Illuminate\Http\Response
-     */
     public function show()
     {
         $game = new Game21();
